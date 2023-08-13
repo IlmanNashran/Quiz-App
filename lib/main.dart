60f1a562fde9640352043617a607448884ac8a82
IlmanNashran <ilmannashran@gmail.com>
2023-08-13 21:40:46 +0800

    create totalscore

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/pages/game_page.dart';
+import 'package:quiz_app/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: GamePage(),
+      home: HomePage(),
     );
   }
 
--- lib/pages/game_page_provider.dart
+++ lib/pages/game_page_provider.dart
@@ -10,6 +10,7 @@ class GamePageProvider extends ChangeNotifier {
 
   List? questions; //get query api input
   int _currentQuestionCount = 0;
+  int _correctCount = 0;
 
   BuildContext context;
 
@@ -42,6 +43,7 @@ class GamePageProvider extends ChangeNotifier {
   void answerQuestion(String _answer) async {
     bool isCorrect =
         questions![_currentQuestionCount]["correct_answer"] == _answer;
+    _correctCount += isCorrect ? 1 : 0;
     _currentQuestionCount++;
     showDialog(
       context: context,
@@ -70,13 +72,13 @@ class GamePageProvider extends ChangeNotifier {
     showDialog(
       context: context,
       builder: (BuildContext _context) {
-        return const AlertDialog(
+        return AlertDialog(
           backgroundColor: Colors.blue,
-          title: Text(
+          title: const Text(
             "End Game!",
             style: TextStyle(fontSize: 25, color: Colors.white),
           ),
-          content: Text("Score:0/0"),
+          content: Text("Score: $_correctCount/$_maxQuestions"),
         );
       },
     );
