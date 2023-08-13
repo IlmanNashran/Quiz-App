ec07fe45b7836a80f52e7c8fd0d59229850aa668
IlmanNashran <ilmannashran@gmail.com>
2023-08-13 12:47:40 +0800

    create answering question function

--- lib/pages/game_page_provider.dart
+++ lib/pages/game_page_provider.dart
@@ -38,4 +38,12 @@ class GamePageProvider extends ChangeNotifier {
   String getCurrentQuestions() {
     return questions![_currentQuestionCount]["question"];
   }
+
+  void answerQuestion(String _answer) async {
+    bool isCorrect =
+        questions![_currentQuestionCount]["correct_answer"] == _answer;
+    _currentQuestionCount++;
+    print(isCorrect ? "correct" : "inCorrect");
+    notifyListeners();
+  }
 }
